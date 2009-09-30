77b5da03a6e9edebaaa85261b05c7450526a3912
Richard Patching <richard@edendevelopment.co.uk>
2009-09-30 08:52:23 +0100

    Scrolling collision a lot better now.

--- Examples/collision_example_viewport.rb
+++ Examples/collision_example_viewport.rb
@@ -27,17 +27,19 @@ class CollisionExampleWindow < Gosu::Window
     @map = SpriteJam::TileMap.new(self, 'map2.map', 800, 600)
     @player = Player.new(self, 20, 20)
     @hud = Gosu::Image.new(self, 'hud_1.png', false)
-    @coord_text = Gosu::Font.new(self, Gosu::default_font_name, 30) 
+    @coord_text = Gosu::Font.new(self, Gosu::default_font_name, 30)
     @scroll_x = 0
     @scroll_y = 0
   end
   
   def update
     if button_down? Gosu::Button::KbRight
-      @player.x += 3 unless @map.solid_tile?('right', @player.x, @player.y, 0, 0)
-      # if @player.x >= 400
-      #         @scroll_x += 1 unless ((@scroll_x + width) >= @map.width)
-      #       end
+      if !@map.solid_tile?('right', @player.x, @player.y, @scroll_x, 0)
+        @player.x += 3
+        if @player.x >= 400
+           @scroll_x += 1 unless (((@scroll_x * 32) + width) >= @map.width)
+        end
+      end
     end
     
     if button_down? Gosu::Button::KbLeft
@@ -54,12 +56,12 @@ class CollisionExampleWindow < Gosu::Window
   end
   
   def draw
-    @coord_text.draw("#{@player.x}", 70, 525, 1, 1.0, 1.0, 0xffffffff)
+    @coord_text.draw("#{@player.x + (@scroll_x*32)}", 70, 525, 1, 1.0, 1.0, 0xffffffff)
     @coord_text.draw("#{@player.y}", 210, 525, 1, 1.0, 1.0, 0xffffffff)
+    @coord_text.draw("#{@scroll_x}", 500, 525, 1, 1.0, 1.0, 0xffffffff)
     @map.draw(@scroll_x, 0)
     @player.draw
     @hud.draw(0,500,0)
-    
   end
   
 end
