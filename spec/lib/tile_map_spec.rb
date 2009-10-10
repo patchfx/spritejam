e17804fe3dbab2272036307ff579ae7dee03b095
Richard Patching <richard@edendevelopment.co.uk>
2009-10-10 21:06:49 +0100

    Start specs for tile map.

--- spec/lib/tile_map_spec.rb
+++ spec/lib/tile_map_spec.rb
@@ -1,7 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe SpriteJam::TileMap do
-  it "should do something" do
-    t = SpriteJam::TileMap.new
+  describe "creation" do
+    before(:each) do
+      SpriteJam::TileSet.stub!(:new)
+    end
+    
+    it "loads a map file" do
+      SpriteJam::Map.should_receive(:new).with('Foo').and_return(mock(:map))
+      SpriteJam::TileMap.new(mock(:window), 'Foo', 300, 300)
+    end
+    
+    it "loads a tile set" do
+      window = mock(:window)
+      map = mock(:map)
+      SpriteJam::Map.stub!(:new).and_return(map)
+      SpriteJam::TileSet.should_receive(:new).with(window, map).and_return(mock(:tileset))
+      SpriteJam::TileMap.new(window, 'Foo', 300, 300)
+    end
   end
 end
